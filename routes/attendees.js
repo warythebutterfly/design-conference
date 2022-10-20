b02fedeedd19298e78e27393a463ce2241ee4c33
Raheem Abdulhaqq <raheemabdulhaqq2020@gmail.com>
2022-10-20 05:10:08 +0100

    Pulled and passed data from db to html table

--- routes/attendees.js
+++ routes/attendees.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const Attendee = require("../models/register.model");
 const app = express.Router();
 
 //call for the main schedule page
-app.get("/", (req, res) => {
-  res.render("attendees");
+app.get("/", async (req, res) => {
+  try {
+    attendees = await Attendee.find().sort({ "Name": 1 })
+    res.render("attendees", {
+      attendees: attendees
+    });
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 module.exports = app;
